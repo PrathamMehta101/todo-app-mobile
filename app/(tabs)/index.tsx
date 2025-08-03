235288f3e02736e72e945c972a88ad140527b61e
PrathamMehta101 <pratham10mehta@gmail.com>
2025-08-03 22:07:59 +0530

    added convex database

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,13 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos);
 
   return (
     <View style={styles.container}>
